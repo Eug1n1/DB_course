8bdba396277a52405e4c2080b5d14cbcf695e327
Shumskiy Evgeniy <vkzheny@gmail.com>
2022-12-06 14:03:34 +0600

    install.bat

---
+++ install.bat
@@ -0,0 +1,2 @@
+mkdir C:\DB_course
+SQLCMD.EXE -E -S localhost -i .\create_tables.sql .\indexes.sql .\insert.sql .\select.sql .\update.sql .\delete.sql .\other_procedures.sql .\export_import.sql .\store.sql .\auth.sql .\backup_DB.sql .\enable_encription.sql .\auto_insert.sql
