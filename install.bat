ab797e95d0f50035402b7a8fe4589e10a9b84560
Shumskiy Evgeniy <vkzheny@gmail.com>
2022-12-07 06:53:50 +0600

    fix paths

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
 mkdir C:\DB_course
-SQLCMD.EXE -E -S localhost -i .\create_tables.sql .\indexes.sql .\insert.sql .\select.sql .\update.sql .\delete.sql .\other_procedures.sql .\export_import.sql .\store.sql .\auth.sql .\backup_DB.sql .\enable_encription.sql .\auto_insert.sql
+SQLCMD.EXE -E -S localhost -i .\scripts\create_tables.sql .\scripts\indexes.sql .\scripts\insert.sql .\scripts\select.sql .\scripts\update.sql .\scripts\delete.sql .\scripts\other_procedures.sql .\scripts\export_import.sql .\scripts\store.sql .\scripts\auth.sql .\scripts\backup_DB.sql .\scripts\enable_encription.sql .\scripts\auto_insert.sql
